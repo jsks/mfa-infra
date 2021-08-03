deb032ed0991e57abf78ce83680fa8141aefa911
Joshua Krusell <js.shirin@gmail.com>
2021-08-03 16:43:11 +0200

    Rename server hostnames

--- terraform/appserver.tf
+++ terraform/appserver.tf
@@ -4,23 +4,23 @@ resource "openstack_blockstorage_volume_v2" "db_vol" {
   size        = 900
 }
 
-resource "openstack_compute_instance_v2" "app_server" {
-  name            = "app-server"
+resource "openstack_compute_instance_v2" "appserver" {
+  name            = "appserver"
   flavor_name     = "ssc.medium.highcpu"
   image_name      = "Debian 10 (Buster) - latest"
   security_groups = ["${openstack_networking_secgroup_v2.secgroup.name}"]
   user_data       = file("./appserver_data.yaml")
   network {
-    name = "SNIC 2021/18-3 Internal IPv4 Network"
+    name = var.network_name
   }
 }
 
 resource "openstack_compute_volume_attach_v2" "attach_vol" {
-  instance_id = openstack_compute_instance_v2.app_server.id
+  instance_id = openstack_compute_instance_v2.appserver.id
   volume_id   = openstack_blockstorage_volume_v2.db_vol.id
   device      = "/dev/vdb"
 }
 
 output "private_ip" {
-  value = openstack_compute_instance_v2.app_server.network[0].fixed_ip_v4
+  value = openstack_compute_instance_v2.appserver.network[0].fixed_ip_v4
 }
--- terraform/template.tf
+++ terraform/template.tf
@@ -1,7 +1,7 @@
 resource "local_file" "inventory" {
   content = templatefile("inventory.tpl", {
-    appserver = openstack_compute_instance_v2.app_server.network[0].fixed_ip_v4,
-    monitor   = openstack_networking_floatingip_v2.public_ip.address,
+    appserver = openstack_compute_instance_v2.appserver.network[0].fixed_ip_v4,
+    gateway   = openstack_networking_floatingip_v2.public_ip.address,
   })
   filename = "../inventory"
 }
